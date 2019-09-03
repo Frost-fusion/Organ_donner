5e4fe7a0804ba924fc3dfbc239e5f4b1bc652d47
Frost-fusion <prashisborkar1999@gmail.com>
2019-09-03 23:17:45 +0530

    added database construction in .py file completed ui file upto 70%

--- load.py
+++ load.py
@@ -1,53 +1,59 @@
-import sys
-from PyQt5 import QtWidgets, uic
+import datetime
+import re
 import sqlite3
+import sys
+
+from PyQt5 import QtWidgets, uic
 
 
-class test:
+class Test:
     def __init__(self):
         self.ui = uic.loadUi('organ.ui')
         self.ui.show()
-        self.db()
-        self.ui.tabWidget.setCurrentIndex(0)
-        self.ui.log.clicked.connect(self.logit)
-        self.ui.reg.clicked.connect(self.tabchange)
+        self.create_database()
+        self.ui.mainTab.setCurrentIndex(0)
+        self.ui.loginButton.clicked.connect(self.login)
+        self.ui.homeTabRegisterButton.clicked.connect(self.tabchange)
+        self.ui.changeFormStack.setCurrentIndex(self.ui.formSelecter.currentIndex())
+        self.ui.registerTabRegisterButton.clicked.connect(self.addRecord)
 
-    def logit(self):
-        global usr
-        global psd
-        self.ui.usr_name1.setText('')
-        self.ui.usr_pass1.setText('')
+    def login(self):
+        global user_name
+        global password
+        self.ui.statusNameLabel.setText('')
+        self.ui.statusPasswordLabel.setText('')
         try:
-            usr = self.ui.usr_name.text()
-            psd = self.ui.usr_pass.text()
-            if usr == "" and psd == "":
+            user_name = self.ui.userName.text()
+            password = self.ui.userPassword.text()
+            if user_name == "" and password == "":
                 raise Exception('both')
-            if usr == "":
-                raise Exception('usr')
-            if psd == "":
-                raise Exception('psd')
+            if user_name == "":
+                raise Exception('user_name')
+            if password == "":
+                raise Exception('password')
         except Exception as e:
             if str(e) == 'both':
-                self.ui.usr_name1.setText('* UserName Required')
-                self.ui.usr_pass1.setText('* Password Required')
-            if str(e) == 'usr':
-                self.ui.usr_name1.setText('* UserName Required')
-            if str(e) == 'psd':
-                self.ui.usr_pass1.setText('* Password Required')
+                self.ui.statusNameLabel.setText('* UserName Required')
+                self.ui.statusPasswordLabel.setText('* Password Required')
+            if str(e) == 'user_name':
+                self.ui.statusNameLabel.setText('* UserName Required')
+            if str(e) == 'password':
+                self.ui.statusPasswordLabel.setText('* Password Required')
         else:
-            self.cur.execute('select * from login_id where usr_name = ? and password = ?',((usr),(psd)))
-            chk = self.cur.fetchall()
+            self.cursor.execute('select * from login_id where usr_name = ? and password = ?;',
+                                ((user_name), (password)))
+            chk = self.cursor.fetchall()
             if chk:
                 self.ui.statusbar.showMessage("Login Successful")
             else:
-                self.ui.usr_name1.setText('Incorrect Username or Password')
+                self.ui.statusNameLabel.setText('Incorrect Username or Password')
 
-    def db(self):
+    def create_database(self):
         try:
-            self.conn = sqlite3.connect('organRecord')
-            self.cur = self.conn.cursor()
-            self.ui.statusbar.showMessage("Connected to Database")
+            self.connection = sqlite3.connect('organRecord.db')
+            self.cursor = self.connection.cursor()
             self.createdata()
+            self.ui.statusbar.showMessage("Connected to Database")
         except Exception as e:
             print(e)
             self.ui.statusbar.showMessage(" Failed to Connect to Database:   Exiting Application")
@@ -55,24 +61,137 @@ class test:
 
     def createdata(self):
         try:
-            smt = "CREATE TABLE 'donor_data' ( `reg_no` TEXT NOT NULL UNIQUE, ` fname` TEXT, `mname` TEXT, `lname` TEXT, `dob` DATE, `Gender` NUMERIC, `Blood group` TEXT, `mob_no` INTEGER NOT NULL, `tele_no` INTEGER, `email` TEXT, `address` TEXT, `city` TEXT, `state` TEXT, `district` TEXT, `pin` NUMERIC,`pledge_date` DATE, `organ_donate` TEXT PRIMARY KEY(`reg_no`) );"
-            self.cur.execute(smt)
-            smt = "CREATE TABLE 'reciver_data' ( `reg_no` TEXT NOT NULL UNIQUE, ` fname` TEXT, `mname` TEXT, `lname` TEXT, `dob` DATE, `gender` INTEGER, `Blood group` TEXT, `mob_no.` INTEGER NOT NULL, `tele_no.` INTEGER, `email` TEXT, `address` TEXT,`city` TEXT, `state` TEXT, `district` TEXT, `pin` NUMERIC, `disease_of_patient ` TEXT, `examing_medical_consultant` TEXT, `Doctor_reg_no.` TEXT, `stage` TEXT, `priority ` TEXT, `transplant` TEXT, PRIMARY KEY(`reg_no`) );"
-            self.cur.execute(smt)
-            smt = "CREATE TABLE 'login_id' ( `usr_name` TEXT NOT NULL, `password` TEXT NOT NULL );"
-            self.cur.execute(smt)
-            self.cur.execute("INSERT INTO `login_id`(`usr_name`,`password`) VALUES ('Admin','Admin@00');")
-        except Exception as a:
-            st = str(a)
-            st = st.find('already exists')
-            if st ==-1:
-                self.ui.statusbar.showMessage("Corrupted data in database")
-            return
+            query = """CREATE TABLE IF NOT EXISTS donor(registrationNumber VARCHAR(5), firstName VARCHAR(20),
+                    middleName VARCHAR(20), lastName VARCHAR(20), dateOfBirth DATE, gender VARCHAR(10), 
+                    bloodGroup  VARCHAR(3), mobileNumber INTEGER, landLineNumber INTEGER, email VARCHAR(100),
+                    address VARCHAR(255), city VARCHAR(30), district VARCHAR(30), state varchar(30), pinCode INTEGER, pledgeDate DATE,
+                    donatingOrgans VARCHAR(100));"""
+            self.cursor.execute(query)
+            self.connection.commit()
+            query = """CREATE TABLE IF NOT EXISTS receiver(registrationNumber VARCHAR(5), firstName VARCHAR(20),
+                    middleName VARCHAR(20), lastName VARCHAR(20), dateOfBirth DATE, gender VARCHAR(10), 
+                    bloodGroup  VARCHAR(3), mobileNumber INTEGER, landLineNumber INTEGER, email VARCHAR(100),
+                    address VARCHAR(255), city VARCHAR(30), district VARCHAR(30),state varchar(30), pinCode INTEGER, disease VARCHAR(50),
+                    doctorName VARCHAR(50), doctorRegistration VARCHAR(30), illnessStage VARCHAR(15), priority VARCHAR(20),
+                    transplantingOrgan VARCHAR(10));"""
+            self.cursor.execute(query)
+            self.connection.commit()
+            query = """CREATE TABLE loginId(userName varchar(10),password varchar(10))"""
+            self.cursor.execute(query)
+            query = """INSERT INTO loginId(userName, password)values('Admin','Admin@0'))"""
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as e:
+            print(e)
 
     def tabchange(self):
-        self.ui.tabWidget.setCurrentIndex(1)
+        self.ui.mainTab.setCurrentIndex(1)
+
+    def addRecord(self):
+        if self.ui.formSelecter.currentIndex() == 0:
+            data = []
+            registration_number = 'DON1'
+            self.cursor.execute("select registrationNumber from donor")
+            if self.cursor.fetchone():
+                self.cursor.execute("select registrationNumber from donor ORDER BY registrationNumber DESC LIMIT 1;")
+                old_registration_number = self.cursor.fetchone()
+                registration_number = int(re.search(r'\d', old_registration_number[0]).group())
+                registration_number = 'DON' + str(registration_number + 1)
+            data.append(registration_number)
+            data.append(self.ui.fristName.text())
+            data.append(self.ui.middleName.text())
+            data.append(self.ui.lastName.text())
+            date_of_birth = self.ui.dateOfBirth.date().toPyDate()
+            data.append(date_of_birth.strftime("%d-%m-%Y"))
+            if self.ui.radioFemale.isChecked():
+                data.append('Female')
+            if self.ui.radioMale.isChecked():
+                data.append('Male')
+            else:
+                data.append('Other')
+            data.append(self.ui.bloodGroup.currentText())
+            data.append(self.ui.mobileNumber.text())
+            data.append(self.ui.landLineNumber.text())
+            data.append(self.ui.email.text())
+            data.append(self.ui.permanantAddress.toPlainText())
+            data.append(self.ui.addCity.text())
+            data.append(self.ui.addState.currentText())
+            data.append(self.ui.addDistrict.text())
+            data.append(self.ui.addPincode.text())
+            pledge_date = datetime.datetime.now()
+            data.append(pledge_date.strftime("%d-%m-%Y-%I-%M-%p"))
+            donate = ''
+            if self.ui.donateBody.isChecked():
+                donate = donate + '-Body'
+            if self.ui.donateEyes.isChecked():
+                donate = donate + '-Eyes'
+            if self.ui.donateKidney.isChecked():
+                donate = donate + '-Kidney'
+            if self.ui.donatePancreas.isChecked():
+                donate = donate + '-Pancreas'
+            if self.ui.donateHeart.isChecked():
+                donate = donate + '-Heart'
+            if self.ui.donateLungs.isChecked():
+                donate = donate + '-Lungs'
+            data.append(donate)
+            query = """INSERT INTO donor(registrationNumber , firstName ,middleName , lastName ,
+                        dateOfBirth , gender , bloodGroup , mobileNumber , landLineNumber , email ,
+                        address , city , district ,state, pinCode , pledgeDate ,donatingOrgans) 
+                        VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"""
+            self.cursor.execute(query, data)
+            self.connection.commit()
+            self.ui.statusbar.showMessage("Added Record")
+        if self.ui.formSelecter.currentIndex() == 1:
+            data = []
+            registration_number = 'DON1'
+            self.cursor.execute("select registrationNumber from receiver")
+            if self.cursor.fetchone():
+                self.cursor.execute("select registrationNumber from receiver ORDER BY registrationNumber DESC LIMIT 1;")
+                old_registration_number = self.cursor.fetchone()
+                registration_number = int(re.search(r'\d+', old_registration_number[0]).group())
+                registration_number = 'REC' + str(registration_number + 1)
+            data.append(registration_number)
+            data.append(self.ui.fristName.text())
+            data.append(self.ui.middleName.text())
+            data.append(self.ui.lastName.text())
+            date_of_birth = self.ui.dateOfBirth.date().toPyDate()
+            data.append(date_of_birth.strftime("%d-%m-%Y"))
+            if self.ui.radioFemale.isChecked():
+                data.append('Female')
+            if self.ui.radioMale.isChecked():
+                data.append('Male')
+            else:
+                data.append('Other')
+            data.append(self.ui.bloodGroup.currentText())
+            data.append(self.ui.mobileNumber.text())
+            data.append(self.ui.landLineNumber.text())
+            data.append(self.ui.email.text())
+            data.append(self.ui.permanantAddress.toPlainText())
+            data.append(self.ui.addCity.text())
+            data.append(self.ui.addState.currentText())
+            data.append(self.ui.addDistrict.text())
+            data.append(self.ui.addPincode.text())
+            data.append(self.ui.patientDisease.text())
+            data.append(self.ui.doctorName.text())
+            data.append(self.ui.doctorRegistartion.text())
+            data.append(self.ui.illnessStage.currentText())
+            data.append(self.ui.patientPriority.currentText())
+            data.append(self.ui.transplantOrgan.currentText())
+            query = """INSERT INTO receiver(registrationNumber , firstName ,middleName , lastName ,
+                        dateOfBirth , gender , bloodGroup , mobileNumber , landLineNumber , email ,
+                        address , city , district , state, pinCode, disease, doctorName, doctorRegistration,illnessStage,
+                        priority, transplantingOrgan) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"""
+            self.cursor.execute(query, data)
+            self.connection.commit()
+
+    def addloginid(self, registration):
+        pass
+
+    def __del__(self):
+        self.connection.commit()
+        self.connection.close()
 
 
 app = QtWidgets.QApplication(sys.argv)
-win = test()
+win = Test()
 sys.exit(app.exec_())
