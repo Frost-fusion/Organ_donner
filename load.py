46203f14f7def19a90f87697554357381367962e
Frost-fusion <prashisborkar1999@gmail.com>
2019-09-09 15:32:28 +0530

    added database construction in .py file completed ui file upto 80%

--- load.py
+++ load.py
@@ -3,23 +3,39 @@ import re
 import sqlite3
 import sys
 
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5 import QtWidgets, uic
 
 
+def password_reset():
+    test = forgot()
+    test.show()
+
 class Test:
+    cursor: sqlite3.Cursor
+
     def __init__(self):
         self.ui = uic.loadUi('organ.ui')
         self.ui.show()
         self.create_database()
         self.ui.mainTab.setCurrentIndex(0)
+        self.ui.changeLoginTypeStack.setCurrentIndex(0)
+        self.ui.changeFormStack.setCurrentIndex(0)
+        self.ui.adminLoginStackTab.setCurrentIndex(0)
         self.ui.loginButton.clicked.connect(self.login)
-        self.ui.homeTabRegisterButton.clicked.connect(self.tabchange)
-        self.ui.changeFormStack.setCurrentIndex(self.ui.formSelecter.currentIndex())
-        self.ui.registerTabRegisterButton.clicked.connect(self.addRecord)
+        self.ui.homeTabRegisterButton.clicked.connect(self.tab_change)
+        self.ui.formSelecter.currentIndexChanged.connect(self.change_form)
+        self.ui.registerTabRegisterButton.clicked.connect(self.add_record)
+        self.ui.renewApplicationButton.clicked.connect(self.renew_application)
+        self.ui.goToLoginPageReceiver.clicked.connect(self.set_login_page)
+        self.ui.goToLoginPageDonor.clicked.connect(self.set_login_page)
+        self.ui.goToLoginPageAdmin.clicked.connect(self.set_login_page)
+        self.ui.forgotPasswordButton.clicked.connect(password_reset)
+        self.ui.adminLoginStackTab.currentChanged.connect(self.populate_record)
 
     def login(self):
-        global user_name
-        global password
+        user_name: str
+        password: str
         self.ui.statusNameLabel.setText('')
         self.ui.statusPasswordLabel.setText('')
         try:
@@ -40,11 +56,27 @@ class Test:
             if str(e) == 'password':
                 self.ui.statusPasswordLabel.setText('* Password Required')
         else:
-            self.cursor.execute('select * from login_id where usr_name = ? and password = ?;',
-                                ((user_name), (password)))
-            chk = self.cursor.fetchall()
-            if chk:
-                self.ui.statusbar.showMessage("Login Successful")
+            check = None
+            if 'Admin' in user_name:
+                self.cursor.execute('select * from loginId where username = ? and password = ?;',
+                                    (user_name, password))
+                check = self.cursor.fetchall()
+                if check:
+                    self.populate_record()
+                    self.ui.changeLoginTypeStack.setCurrentIndex(1)
+            elif 'REC' in user_name:
+                self.cursor.execute('select * from loginId where userName = ? and password = ?;',
+                                    (user_name, password))
+                check = self.cursor.fetchall()
+                if check:
+                    self.ui.changeLoginTypeStack.setCurrentIndex(3)
+                    self.populate_receiver(user_name)
+            elif 'DON' in user_name:
+                self.cursor.execute('select * from loginId where userName = ? and password = ?;',
+                                    (user_name, password))
+                check = self.cursor.fetchall()
+                if check:
+                    self.ui.changeLoginTypeStack.setCurrentIndex(2)
             else:
                 self.ui.statusNameLabel.setText('Incorrect Username or Password')
 
@@ -52,46 +84,54 @@ class Test:
         try:
             self.connection = sqlite3.connect('organRecord.db')
             self.cursor = self.connection.cursor()
-            self.createdata()
+            self.create_data()
             self.ui.statusbar.showMessage("Connected to Database")
         except Exception as e:
             print(e)
             self.ui.statusbar.showMessage(" Failed to Connect to Database:   Exiting Application")
             app.exit()
 
-    def createdata(self):
+    def create_data(self):
         try:
             query = """CREATE TABLE IF NOT EXISTS donor(registrationNumber VARCHAR(5), firstName VARCHAR(20),
                     middleName VARCHAR(20), lastName VARCHAR(20), dateOfBirth DATE, gender VARCHAR(10), 
-                    bloodGroup  VARCHAR(3), mobileNumber INTEGER, landLineNumber INTEGER, email VARCHAR(100),
-                    address VARCHAR(255), city VARCHAR(30), district VARCHAR(30), state varchar(30), pinCode INTEGER, pledgeDate DATE,
-                    donatingOrgans VARCHAR(100));"""
+                    bloodGroup  VARCHAR(3), mobileNumber VARCHAR(15), landLineNumber VARCHAR(15), email VARCHAR(100),
+                    address VARCHAR(255), city VARCHAR(30), district VARCHAR(30), state varchar(30), pinCode VARCHAR(6),
+                    pledgeDate DATE,donatingOrgans VARCHAR(100));"""
             self.cursor.execute(query)
             self.connection.commit()
             query = """CREATE TABLE IF NOT EXISTS receiver(registrationNumber VARCHAR(5), firstName VARCHAR(20),
                     middleName VARCHAR(20), lastName VARCHAR(20), dateOfBirth DATE, gender VARCHAR(10), 
-                    bloodGroup  VARCHAR(3), mobileNumber INTEGER, landLineNumber INTEGER, email VARCHAR(100),
-                    address VARCHAR(255), city VARCHAR(30), district VARCHAR(30),state varchar(30), pinCode INTEGER, disease VARCHAR(50),
-                    doctorName VARCHAR(50), doctorRegistration VARCHAR(30), illnessStage VARCHAR(15), priority VARCHAR(20),
-                    transplantingOrgan VARCHAR(10));"""
+                    bloodGroup  VARCHAR(3), mobileNumber VARCHAR(15), landLineNumber VARCHAR(15), email VARCHAR(100),
+                    address VARCHAR(255), city VARCHAR(30), district VARCHAR(30),state varchar(30), pinCode VARCHAR(6),
+                    disease VARCHAR(50),doctorName VARCHAR(50), doctorRegistration VARCHAR(30), illnessStage VARCHAR(15)
+                    ,priority VARCHAR(20),transplantingOrgan VARCHAR(10));"""
             self.cursor.execute(query)
             self.connection.commit()
-            query = """CREATE TABLE loginId(userName varchar(10),password varchar(10))"""
+            query = """CREATE TABLE IF NOT EXISTS loginId(userName varchar(10),password varchar(10));"""
             self.cursor.execute(query)
-            query = """INSERT INTO loginId(userName, password)values('Admin','Admin@0'))"""
+            query = """SELECT * FROM loginId;"""
             self.cursor.execute(query)
-            self.connection.commit()
+            if not self.cursor.fetchone():
+                query = """INSERT INTO loginId(userName, password) values('Admin','Admin@0');"""
+                self.cursor.execute(query)
+                self.connection.commit()
         except Exception as e:
             print(e)
 
-    def tabchange(self):
+    def tab_change(self):
         self.ui.mainTab.setCurrentIndex(1)
 
-    def addRecord(self):
+    def set_login_page(self):
+        self.ui.changeLoginTypeStack.setCurrentIndex(0)
+        self.ui.userName.clear()
+        self.ui.userPassword.clear()
+
+    def add_record(self):
         if self.ui.formSelecter.currentIndex() == 0:
             data = []
             registration_number = 'DON1'
-            self.cursor.execute("select registrationNumber from donor")
+            self.cursor.execute("select registrationNumber from donor;")
             if self.cursor.fetchone():
                 self.cursor.execute("select registrationNumber from donor ORDER BY registrationNumber DESC LIMIT 1;")
                 old_registration_number = self.cursor.fetchone()
@@ -122,17 +162,17 @@ class Test:
             data.append(pledge_date.strftime("%d-%m-%Y-%I-%M-%p"))
             donate = ''
             if self.ui.donateBody.isChecked():
-                donate = donate + '-Body'
+                donate = donate + 'Body'
             if self.ui.donateEyes.isChecked():
-                donate = donate + '-Eyes'
+                donate = donate + 'Eyes'
             if self.ui.donateKidney.isChecked():
-                donate = donate + '-Kidney'
+                donate = donate + 'Kidney'
             if self.ui.donatePancreas.isChecked():
-                donate = donate + '-Pancreas'
+                donate = donate + 'Pancreas'
             if self.ui.donateHeart.isChecked():
-                donate = donate + '-Heart'
+                donate = donate + 'Heart'
             if self.ui.donateLungs.isChecked():
-                donate = donate + '-Lungs'
+                donate = donate + 'Lungs'
             data.append(donate)
             query = """INSERT INTO donor(registrationNumber , firstName ,middleName , lastName ,
                         dateOfBirth , gender , bloodGroup , mobileNumber , landLineNumber , email ,
@@ -141,10 +181,13 @@ class Test:
             self.cursor.execute(query, data)
             self.connection.commit()
             self.ui.statusbar.showMessage("Added Record")
+            response = add_sucess()
+            if response:
+                self.renew_application()
         if self.ui.formSelecter.currentIndex() == 1:
             data = []
             registration_number = 'DON1'
-            self.cursor.execute("select registrationNumber from receiver")
+            self.cursor.execute("select registrationNumber from receiver;")
             if self.cursor.fetchone():
                 self.cursor.execute("select registrationNumber from receiver ORDER BY registrationNumber DESC LIMIT 1;")
                 old_registration_number = self.cursor.fetchone()
@@ -173,7 +216,7 @@ class Test:
             data.append(self.ui.addPincode.text())
             data.append(self.ui.patientDisease.text())
             data.append(self.ui.doctorName.text())
-            data.append(self.ui.doctorRegistartion.text())
+            data.append(self.ui.doctorRegistration.text())
             data.append(self.ui.illnessStage.currentText())
             data.append(self.ui.patientPriority.currentText())
             data.append(self.ui.transplantOrgan.currentText())
@@ -183,13 +226,122 @@ class Test:
                         priority, transplantingOrgan) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"""
             self.cursor.execute(query, data)
             self.connection.commit()
+            response = add_sucess()
+            if response:
+                self.renew_application()
 
-    def addloginid(self, registration):
+    def change_form(self):
+        self.ui.changeFormStack.setCurrentIndex(self.ui.formSelecter.currentIndex())
+
+    def add_login_id(self, registration):
         pass
+
+    def populate_record(self):
+        try:
+            donor_query = """select * from donor;"""
+            self.cursor.execute(donor_query)
+            donor_table = self.cursor.fetchall()
+            required_rows = len(donor_table)
+            self.ui.adminLoginStackDonorTable.setRowCount(required_rows)
+            x = -1
+            for i in donor_table:
+                x = x + 1
+                for j in range(0, 17):
+                    self.ui.adminLoginStackDonorTable.setItem(x, j, QtWidgets.QTableWidgetItem(i[j]))
+
+            receiver_query = """select * from receiver;"""
+            self.cursor.execute(receiver_query)
+            receiver_table = self.cursor.fetchall()
+            required_rows = len(receiver_table)
+            self.ui.adminLoginStackReceiverTable.setRowCount(required_rows)
+            x = -1
+            for i in receiver_table:
+                x = x + 1
+                for j in range(0, 21):
+                    self.ui.adminLoginStackReceiverTable.setItem(x, j, QtWidgets.QTableWidgetItem(i[j]))
+        except Exception as e:
+            print(e)
+
+    def populate_receiver(self, user_id):
+        try:
+            query = """select registrationNumber,priority,bloodGroup,doctorName from receiver;"""
+            self.cursor.execute(query)
+            waiting_table = self.cursor.fetchall()
+            required_rows = len(waiting_table)
+            self.ui.receiverLoginStackTable.setRowCount(required_rows)
+            x = -1
+            for i in waiting_table:
+                x = x + 1
+                for j in range(0, 5):
+                    self.ui.receiverLoginStackTable.setItem(x, j, QtWidgets.QTableWidgetItem(i[j]))
+        except Exception as e:
+            print(e)
 
     def __del__(self):
         self.connection.commit()
         self.connection.close()
+
+    def renew_application(self):
+        self.ui.fristName.clear()
+        self.ui.middleName.clear()
+        self.ui.lastName.clear()
+        self.ui.bloodGroup.clear()
+        self.ui.mobileNumber.clear()
+        self.ui.landLineNumber.clear()
+        self.ui.email.clear()
+        self.ui.permanantAddress.clear()
+        self.ui.addCity.clear()
+        self.ui.addState.clear()
+        self.ui.addDistrict.setCurrentIndex(0)
+        self.ui.addPincode.clear()
+        self.ui.patientDisease.clear()
+        self.ui.doctorName.clear()
+        self.ui.doctorRegistration.clear()
+        self.ui.donateBody.setChecked(False)
+        self.ui.donateEyes.setChecked(False)
+        self.ui.donateKidney.setChecked(False)
+        self.ui.donatePancreas.setChecked(False)
+        self.ui.donateHeart.setChecked(False)
+        self.ui.donateLungs.setChecked(False)
+        self.ui.illnessStage.setCurrentIndex(0)
+        self.ui.patientPriority.setCurrentIndex(0)
+        self.ui.transplantOrgan.setCurrentIndex(0)
+
+
+class add_sucess(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'ODMS'
+        self.left = 300
+        self.top = 300
+        self.width = 320
+        self.height = 200
+        self.initUI()
+        self.show()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        buttonReply = QtWidgets.QMessageBox.question(self, 'Registered Successfully', "Want to register another?",
+                                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if buttonReply == QMessageBox.Yes:
+            return True
+        else:
+            return False
+
+
+class forgot(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+         super(forgot, self).__init__(parent)
+         layout = QVBoxLayout()
+         self.b1 = QPushButton("Button1")
+         self.b1.setCheckable(True)
+         self.b1.toggle()
+         self.b1.clicked.connect(lambda: self.whichbtn(self.b1))
+         self.b1.clicked.connect(self.btnstate)
+         layout.addWidget(self.b1)
+
 
 
 app = QtWidgets.QApplication(sys.argv)
